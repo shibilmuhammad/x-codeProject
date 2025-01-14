b772a6fa14f70d4412149375798baaeb75d7b4f1
Shibil Muhammad <shibilmuhammad678@gmail.com>
2025-01-14 14:53:53 +0530

    puppeteer launch updated

--- utils/voucher.js
+++ utils/voucher.js
@@ -29,7 +29,7 @@ async function generatePdfFromHtml(req) {
   
       const browser = await puppeteer.launch({
         headless: true,
-        executablePath: '/usr/bin/google-chrome-stable',
+        executablePath: '/usr/bin/google-chrome',
         args: ['--no-sandbox', '--disable-setuid-sandbox'],
     });
       const page = await browser.newPage();
